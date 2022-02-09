214a4107e8cebb2a6b7ff8d5ccf7cc1806a9f10d
Marta Tsiruh <martatsiruch@gmail.com>
2022-02-09 18:28:02 +0100

    add task 2

---
+++ src/js/01-rest.js
@@ -0,0 +1,51 @@
+import articlesTpl from '../templates/articles.hbs';
+import '../css/news-service.css';
+import NewsApiService from './news-service';
+import LoadMoreBtn from './components/load-more-btn';
+
+const refs = {
+    searchForm: document.querySelector('.js-search-form'),
+    articlesContainer: document.querySelector('.js-articles-container'),
+};
+
+const loadMoreBtn = new LoadMoreBtn({
+    selector: '[data-action="load-more"]',
+    hidden: true,
+});
+
+const newsApiService = new NewsApiService();
+
+refs.searchForm.addEventListener('submit', onSearch);
+loadMoreBtn.refs.button.addEventListener('click', fetchArticles);
+
+
+function onSearch(e) {
+    e.preventDefault();
+
+    newsApiService.query = e.currentTarget.elements.query.value;
+
+    if (newsApiService.query === '') {
+        return alert('Введи что-то нормальное');
+    }
+
+    loadMoreBtn.show();
+    newsApiService.resetPage();
+    clearArticlesContainer();
+    fetchArticles();
+}
+
+function fetchArticles() {
+    loadMoreBtn.disable();
+    newsApiService.fetchArticles().then(articles => {
+        appendArticlesMarkup(articles);
+        loadMoreBtn.enable();
+    });
+}
+
+function appendArticlesMarkup(articles) {
+    refs.articlesContainer.insertAdjacentHTML('beforeend', articlesTpl(articles));
+}
+
+function clearArticlesContainer() {
+    refs.articlesContainer.innerHTML = '';
+}
